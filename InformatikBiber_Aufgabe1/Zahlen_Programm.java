8a1828e66f0377c8e82eaf8af6d72ee58cab280b
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2020-09-03 11:04:27 +0200

    complete copying of files to EFI folder

---
+++ InformatikBiber_Aufgabe1/Zahlen_Programm.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JOptionPane;
+/**
+ * Beschreiben Sie hier die Klasse Zahlen_Programm.
+ * 
+ * @author Jonah Sebright 
+ * @version 14.11.2019
+ */
+public class Zahlen_Programm
+{
+      public static void main(String[] args){
+          
+          //Eingabe Zahl
+            JOptionPane.showInputDialog("Bitte geben Sie ihre Anfangszahl ein");
+          
+          
+        }
+
+}
